8efba70c54b3bdce198feb06685e3f577b83056e
ben <benrobertson9876@gmail.com>
2023-11-07 09:42:23 +1100

    push shared module

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include shared_module
+class shared_module {
+  file { '/tmp/shared.txt':
+    content => 'This content is from the shared module.',
+  }
+}
