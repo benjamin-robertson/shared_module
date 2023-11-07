fa0e78d5f8e6e5551a94880e896c9fa3cc15d995
ben <benrobertson9876@gmail.com>
2023-11-07 16:56:50 +1100

    with slow spec tests

--- spec/classes/shared_module_spec.rb
+++ spec/classes/shared_module_spec.rb
@@ -7,7 +7,28 @@ describe 'shared_module' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile.with_all_deps }
+      tiers = [ 'dog', 'cat', 'blah']
+      datacentres = [ 'blah', 'cats', 'dogs', 'hi', 'hello', 'gday' ]
+
+      tiers.each do |tier|
+        context "with tier #{tier}" do
+          let(:facts) do
+            super().merge({ 'tier' => tier })
+          end
+        
+          datacentres.each do |datacentre|
+            context "with datcentre #{datacentre}" do
+              let(:facts) do
+                super().merge({ 'datacentre' => datacentre })
+              end
+
+              it { is_expected.to compile.with_all_deps }
+              it { is_expected.to contain_file('/tmp/shared.txt').with_content('This content is from the shared module.')}
+              it { is_expected.to contain_file('/blah.txt').with_content(%r{blah})}
+            end
+          end
+        end
+      end
     end
   end
 end
